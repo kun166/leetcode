8d3a6c8a58a2cbe9c2c665db4506ec5cefc5bff9
qinfajia <qinfajia@58.com>
2022-02-15 16:21:45 +0800

    修改了代码

---
+++ src/main/java/com/test/leetcode/erery/Solution1020.java
@@ -0,0 +1,25 @@
+package com.test.leetcode.erery;
+
+/**
+ * @ClassName: Solution1020
+ * @Description:
+ * @Author: qinfajia
+ * @Date: 2022/2/12 10:58
+ * @Version: 1.0
+ */
+public class Solution1020 {
+
+    public int numEnclaves(int[][] grid) {
+        int m = grid.length; // 行数
+        int n = grid[0].length; // 列数
+        // 从上往下,从左到右。联通的置为2，判断不了的不变
+        for (int i = 0; i < m; i++) {
+            for (int j = i; j < n; j++) {
+
+            }
+        }
+
+        // 从下往上，从右到左
+        return 0;
+    }
+}
