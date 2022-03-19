f1bc14e9a02635ff130ad9f8bf49edcdf4ddd51e
qinfajia <qinfajia@58.com>
2022-03-19 09:26:40 +0800

    修改了代码

---
+++ src/main/java/com/test/leetcode/page1/chapter40/t46/Solution20220318.java
@@ -0,0 +1,44 @@
+package com.test.leetcode.page1.chapter40.t46;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: Solution20220318
+ * @Description:
+ * @Author: qinfajia
+ * @Date: 2022/3/18 17:28
+ * @Version: 1.0
+ */
+public class Solution20220318 {
+
+    @Test
+    public void test() {
+        System.out.println(JSON.toJSONString(permute(new int[]{1, 2, 3})));
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ant = new ArrayList<>();
+        permute(nums, ant, new ArrayList<>());
+        return ant;
+    }
+
+    public void permute(int[] nums, List<List<Integer>> ant, List<Integer> list) {
+        if (list.size() == nums.length) {
+            ant.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int number = nums[(i + list.size()) % nums.length];
+            if (list.contains(number)) {
+                continue;
+            }
+            list.add(number);
+            permute(nums, ant, list);
+            list.remove(list.size() - 1);
+        }
+    }
+}
