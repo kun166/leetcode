7f45c2351b4e1b7d364f187d6d42b7cfe0b8220a
qinfajia <qinfajia@58.com>
2022-06-23 10:15:37 +0800

    修改了代码

--- src/main/java/com/test/leetcode/page1/chapter0/t7/OfficialSolution.java
+++ src/main/java/com/test/leetcode/page1/chapter0/t7/OfficialSolution.java
@@ -14,6 +14,7 @@ public class OfficialSolution {
         while (x != 0) {
             int pop = x % 10;
             x /= 10;
+            // Integer.MAX_VALUE值为2147483647，所以rev == Integer.MAX_VALUE / 10永远为假，后面这个判断条件可以省略掉了
             if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
             if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
             rev = rev * 10 + pop;
