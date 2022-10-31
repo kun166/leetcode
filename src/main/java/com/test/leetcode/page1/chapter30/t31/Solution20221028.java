559783266c8d65c0e203720773ee8d9c1b4bfd83
qinfajia <qinfajia@58.com>
2022-10-31 10:02:38 +0800

    修改了代码

--- src/main/java/com/test/leetcode/page1/chapter30/t31/Solution20221028.java
+++ src/main/java/com/test/leetcode/page1/chapter30/t31/Solution20221028.java
@@ -1,5 +1,8 @@
 package com.test.leetcode.page1.chapter30.t31;
 
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+
 /**
  * @ClassName: Solution20221028
  * @Description:
@@ -9,6 +12,13 @@ package com.test.leetcode.page1.chapter30.t31;
  */
 public class Solution20221028 {
 
+    @Test
+    public void test() {
+        int[] array = new int[]{2, 3, 1};
+        nextPermutation(array);
+        System.out.println(JSON.toJSONString(array));
+    }
+
     public void nextPermutation(int[] nums) {
         // 先找到顺序的那个节点
         int index = nums.length - 2;
@@ -16,6 +26,30 @@ public class Solution20221028 {
             if (nums[index] < nums[index + 1]) {
                 break;
             }
+            index--;
         }
+        // 找到比这个节点大的
+        if (index > -1) {
+            for (int i = nums.length - 1; i >= 0; i--) {
+                if (nums[i] > nums[index]) {
+                    // 交换位置
+                    swap(nums, i, index);
+                    break;
+                }
+            }
+        }
+        // index之后的节点交换位置
+        int start = index + 1, end = nums.length - 1;
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    public void swap(int[] nums, int p1, int p2) {
+        int mid = nums[p1];
+        nums[p1] = nums[p2];
+        nums[p2] = mid;
     }
 }
