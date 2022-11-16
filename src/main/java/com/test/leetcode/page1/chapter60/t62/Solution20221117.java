d4ec98a6168f5fb3743bba9915a934ffc4e51201
qinfajia <qinfajia@163.com>
2022-11-17 07:05:30 +0800

    [*]修改了代码

---
+++ src/main/java/com/test/leetcode/page1/chapter60/t62/Solution20221117.java
@@ -0,0 +1,31 @@
+package com.test.leetcode.page1.chapter60.t62;
+
+/**
+ * @ClassName: Solution20221117
+ * @Description:
+ * @Author: qinfajia
+ * @Date: 2022/11/17 6:58
+ * @Version: 1.0
+ */
+public class Solution20221117 {
+
+    public int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    dp[j] = 1;
+                    continue;
+                }
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+}
