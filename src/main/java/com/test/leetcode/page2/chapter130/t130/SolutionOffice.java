07d0f8acc340e2ae523ea238618561e08088dfc9
qinfajia <qinfajia@58.com>
2023-01-29 08:08:09 +0800

    修改了代码

--- src/main/java/com/test/leetcode/page2/chapter130/t130/SolutionOffice.java
+++ src/main/java/com/test/leetcode/page2/chapter130/t130/SolutionOffice.java
@@ -11,23 +11,27 @@ public class SolutionOffice {
     int n, m;
 
     public void solve(char[][] board) {
+        // 行
         n = board.length;
         if (n == 0) {
             return;
         }
+        // 列
         m = board[0].length;
+        // 递归左右两个外边
         for (int i = 0; i < n; i++) {
             // 二维数组的左右两边
             dfs(board, i, 0);
             dfs(board, i, m - 1);
         }
+        // 递归上下两个边
         for (int i = 1; i < m - 1; i++) {
             // 二维数组的上下两边
             dfs(board, 0, i);
             dfs(board, n - 1, i);
         }
 
-        // 遍历二维数组
+        // 遍历二维数组。将联通的'A'改为'O',将非联通的'O'改为'X'
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (board[i][j] == 'A') {
@@ -40,13 +44,18 @@ public class SolutionOffice {
     }
 
     /**
+     * 将该边缘为'O'的修改为'A'。然后递归该坐标的前后左右
+     *
      * @param board 二维数组
-     * @param x     纵坐标
-     * @param y     横坐标
+     * @param x     行
+     * @param y     列
      */
     public void dfs(char[][] board, int x, int y) {
         if (x < 0 || x >= n || y < 0 || y >= m || board[x][y] != 'O') {
             // 如果是超界了，或者该坐标不是'O'，则返回
+            //1,超界
+            //2,该坐标不是'O'
+            // 满足一个即可
             return;
         }
         // 将坐标是'O'且能联通的定为'A'
--- src/main/java/com/test/leetcode/page2/chapter130/t130/SolutionOfficeTwo.java
+++ src/main/java/com/test/leetcode/page2/chapter130/t130/SolutionOfficeTwo.java
@@ -11,48 +11,63 @@ import java.util.Queue;
  */
 public class SolutionOfficeTwo {
 
+    // {x,y}坐标的上下左右四个点
     int[] dx = {1, -1, 0, 0};
     int[] dy = {0, 0, 1, -1};
 
     public void solve(char[][] board) {
+        // 行
         int n = board.length;
         if (n == 0) {
             return;
         }
+        // 列
         int m = board[0].length;
-        // 记录是'O'且没有探测的。坐标。
+        // 记录该坐标是'O'且是联通的，需要探测前后左右四个点位
         // 遍历四个边
         Queue<int[]> queue = new LinkedList<>();
+        //左右两个边
         for (int i = 0; i < n; i++) {
+            // 左边
             if (board[i][0] == 'O') {
                 queue.offer(new int[]{i, 0});
             }
+            // 右边
             if (board[i][m - 1] == 'O') {
                 queue.offer(new int[]{i, m - 1});
             }
         }
+        // 上下两个边
         for (int i = 1; i < m - 1; i++) {
+            // 上边
             if (board[0][i] == 'O') {
                 queue.offer(new int[]{0, i});
             }
+            // 下边
             if (board[n - 1][i] == 'O') {
                 queue.offer(new int[]{n - 1, i});
             }
         }
         while (!queue.isEmpty()) {
+            // 从队列上部取
             int[] cell = queue.poll();
+            // x,y两个轴坐标
             int x = cell[0], y = cell[1];
             // 借助board记录，不用额外记录，哪些元素已经被遍历过了
+            // 联通的修改为'A'
             board[x][y] = 'A';
             for (int i = 0; i < 4; i++) {
                 // 前后左右四个点
                 int mx = x + dx[i], my = y + dy[i];
+                // 如果坐标越界，或者该坐标不为'O',则不处理
                 if (mx < 0 || my < 0 || mx >= n || my >= m || board[mx][my] != 'O') {
                     continue;
                 }
+                // 该坐标是'O'
                 queue.offer(new int[]{mx, my});
             }
         }
+        // 将联通的'A'修改为'O'，将非联通的'O'修改为'A'
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (board[i][j] == 'A') {
