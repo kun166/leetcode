f5d07baeabef5e2e8eaa6808dddc946b850ede8d
qinfajia <qinfajia@58.com>
2023-01-30 17:09:56 +0800

    修改了代码

--- src/main/java/com/test/leetcode/page2/chapter130/t135/Solution.java
+++ src/main/java/com/test/leetcode/page2/chapter130/t135/Solution.java
@@ -11,11 +11,22 @@ import org.junit.Test;
  */
 public class Solution {
 
+    /**
+     * 这代码居然是我当年写出来的？现在写不出来了……
+     * <p>
+     * 感觉这方法，应该是不太可能性的，只能说碰巧通过了代码检测
+     */
     @Test
     public void test() {
         System.out.println(candy(new int[]{29, 51, 87, 87, 72, 12}));
     }
 
+    /**
+     * 找到发一颗糖果的孩子，然后分别向两边分发
+     *
+     * @param ratings
+     * @return
+     */
     public int candy(int[] ratings) {
         if (ratings == null || ratings.length == 0) {
             return 0;
@@ -28,6 +39,7 @@ public class Solution {
         // 初始化一颗糖的位置。该位置评分不高于左右两边评分
         for (int i = 0; i < ratings.length; i++) {
             if (i == 0 || i == ratings.length - 1) {
+                // 第一个孩子和最后一个孩子需要单独处理
                 if (i == 0 && ratings[i] <= ratings[i + 1]) {
                     array[i] = 1;
                     // 向右
@@ -53,11 +65,23 @@ public class Solution {
         return result;
     }
 
+    /**
+     * 计算坐标为index的孩子需要发的糖果数
+     *
+     * @param ratings 评分数组
+     * @param array   糖果发放数量数组
+     * @param index   孩子坐标
+     * @param forward true，是从左向右计算,false是从右向左计算
+     */
     public void candy(int[] ratings, int[] array, int index, boolean forward) {
         while (index >= 0 && index < ratings.length) {
+            // 越界的不处理
             if (forward) {
                 // 向右
+                // 向右发只计算评分比当前孩子高的
                 if (ratings[index] > ratings[index - 1]) {
+                    // 感觉这个还是有问题的，有可能当前坐标孩子糖果已经被计算过了
+                    // array[index] = Math.max(array[index],array[index - 1] + 1); 更好一些
                     array[index] = array[index - 1] + 1;
                     index++;
                 } else {
@@ -66,6 +90,7 @@ public class Solution {
             } else {
                 // 向左
                 if (ratings[index] > ratings[index + 1]) {
+                    // 同上，同样存在问题
                     array[index] = array[index + 1] + 1;
                     index--;
                 } else {
