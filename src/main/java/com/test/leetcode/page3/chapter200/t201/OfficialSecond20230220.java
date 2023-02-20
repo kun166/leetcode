a77c8955252ff655712df03835b5ff3779d8d674
qinfajia <qinfajia@58.com>
2023-02-20 09:08:06 +0800

    修改了代码

--- src/main/java/com/test/leetcode/page3/chapter200/t201/OfficialSecond20230220.java
+++ src/main/java/com/test/leetcode/page3/chapter200/t201/OfficialSecond20230220.java
@@ -11,6 +11,12 @@ public class OfficialSecond20230220 {
 
     /**
      * 方法二：Brian Kernighan 算法
+     * 该方法的前提是理解了解法一，为什么要进行位移。
+     * <p>
+     * Brian Kernighan 算法的关键在于我们每次对 number 和 number−1 之间进行按位与运算后，
+     * number 中最右边的1 会被抹去变成0。
+     * 例如number是1111，则number-1是1110,按位与之后是1110，最右侧的1变成了0
+     * 如number是1100，则number-1是1011,按位与之后是1000
      *
      * @param m
      * @param n
