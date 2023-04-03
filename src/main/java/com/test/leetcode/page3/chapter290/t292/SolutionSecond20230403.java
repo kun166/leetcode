572efb8c28cdaa74aae84f577125066a31c01fe4
qinfajia <qinfajia@58.com>
2023-04-03 17:10:54 +0800

    修改了代码

--- src/main/java/com/test/leetcode/page3/chapter290/t292/SolutionSecond20230403.java
+++ src/main/java/com/test/leetcode/page3/chapter290/t292/SolutionSecond20230403.java
@@ -8,6 +8,15 @@ package com.test.leetcode.page3.chapter290.t292;
  * @Version: 1.0
  */
 public class SolutionSecond20230403 {
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：38.4 MB, 在所有 Java 提交中击败了21%的用户
+     * 通过测试用例：60 / 60
+     *
+     * @param n
+     * @return
+     */
     public boolean canWinNim(int n) {
         return n % 4 != 0;
     }
