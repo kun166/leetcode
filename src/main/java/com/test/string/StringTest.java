9a0114bbe6cf88294170e13c4243dd59f230c7cd
qinfajia <qinfajia@58.com>
2023-04-19 09:58:49 +0800

    修改了代码

--- src/main/java/com/test/string/StringTest.java
+++ src/main/java/com/test/string/StringTest.java
@@ -17,17 +17,17 @@ public class StringTest {
     @Test
     public void testIntern() {
         String str = "执行用时：\n" +
-                "10 ms\n" +
+                "4 ms\n" +
                 ", 在所有 Java 提交中击败了\n" +
-                "92.34%\n" +
+                "100.00%\n" +
                 "的用户\n" +
                 "内存消耗：\n" +
-                "42.8 MB\n" +
+                "46.9 MB\n" +
                 ", 在所有 Java 提交中击败了\n" +
-                "48.08%\n" +
+                "38.57%\n" +
                 "的用户\n" +
                 "通过测试用例：\n" +
-                "168 / 168";
+                "48 / 48";
         System.out.println(str.replaceAll("\n", ""));
     }
 
