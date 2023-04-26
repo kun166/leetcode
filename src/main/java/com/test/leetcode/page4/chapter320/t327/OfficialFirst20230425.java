f6849d1791d476c036a2b40851177edc8c517355
qinfajia <qinfajia@58.com>
2023-04-26 09:41:31 +0800

    修改了代码

--- src/main/java/com/test/leetcode/page4/chapter320/t327/OfficialFirst20230425.java
+++ src/main/java/com/test/leetcode/page4/chapter320/t327/OfficialFirst20230425.java
@@ -9,6 +9,21 @@ package com.test.leetcode.page4.chapter320.t327;
  */
 public class OfficialFirst20230425 {
 
+    /**
+     * 归并排序
+     * <p>
+     * 归并排序的思路：如果我们需要求区间[s,e]的答案，那么如果我们已经知道了[s,mid]和
+     * [mid+1,e]的答案（mid=(s+e)/2），是否有办法得到[s,e]的答案。这里我们设区间[s,mid]
+     * 为 n1，区间[mid+1,e]为 n2，那么合法的数对(i,j)有且只有三种情况：
+     * 1,i和j都出现在​n1中
+     * 2,i和j都出现在​n2中
+     * 3,左端点在n1,右端点在n2，即i在n1,j在n2
+     *
+     * @param nums
+     * @param lower
+     * @param upper
+     * @return
+     */
     public int countRangeSum(int[] nums, int lower, int upper) {
         long s = 0;
         long[] sum = new long[nums.length + 1];
@@ -20,11 +35,19 @@ public class OfficialFirst20230425 {
         return countRangeSumRecursive(sum, lower, upper, 0, sum.length - 1);
     }
 
+    /**
+     * @param sum   数组前缀和
+     * @param lower 下界
+     * @param upper 上届
+     * @param left  左侧起始
+     * @param right 右侧起始
+     * @return
+     */
     public int countRangeSumRecursive(long[] sum, int lower, int upper, int left, int right) {
         if (left == right) {
             return 0;
         } else {
-            // 去中间位置
+            // 选取中间点进行二分？
             int mid = (left + right) / 2;
             int n1 = countRangeSumRecursive(sum, lower, upper, left, mid);
             int n2 = countRangeSumRecursive(sum, lower, upper, mid + 1, right);
