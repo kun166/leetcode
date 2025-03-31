4f35ce80e617fb2180fc85e6d537d1f0c50890e5
qinfajia <qinfajia@58.com>
2025-03-31 10:40:56 +0800

    修改了代码

--- src/main/java/com/test/leetcode/page3/chapter210/t214/Solution2025032804.java
+++ src/main/java/com/test/leetcode/page3/chapter210/t214/Solution2025032804.java
@@ -9,15 +9,22 @@ import org.junit.Test;
  * @Date: 2023/2/24 11:17
  * @Version: 1.0
  */
-public class Solution20230224 {
+public class Solution2025032804 {
 
     @Test
     public void test() {
         System.out.println(shortestPalindrome("a"));
     }
 
-    // 尝试下双指针吧
-    // 呃,20250328发现这解法居然过了……
+    /**
+     * 呃,20250328发现以前的一个解法居然过了?
+     * 自己都不理解了?
+     * <p>
+     * 好吧,优化了一下,居然都 超出时间限制 了……
+     *
+     * @param s
+     * @return
+     */
     public String shortestPalindrome(String s) {
         int left = 0, right = s.length() - 1;
         while (left < right) {
@@ -25,6 +32,7 @@ public class Solution20230224 {
             while (s.charAt(cl) != s.charAt(cr)) {
                 cr--;
             }
+            right = cr - 1;
             while (cl < cr && s.charAt(cl) == s.charAt(cr)) {
                 cl++;
                 cr--;
@@ -38,9 +46,7 @@ public class Solution20230224 {
                 // 说明中间是一个abba的形式
                 return new StringBuffer(s.substring(cl * 2)).reverse() + s;
             }
-            right--;
         }
-        //
         return s;
     }
 }
